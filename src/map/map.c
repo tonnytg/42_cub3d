e68e2eeb4ecf6d081df0cfde619ce16fbaf598ef
lbiasuz <biasuz.l.leonardo@gmail.com>
2023-10-11 16:55:17 +0200

    finalizada normalização do mapa

--- src/map/map.c
+++ src/map/map.c
@@ -12,47 +12,88 @@
 
 #include <cub3D.h>
 
-int	read_map(t_game *game)
+int	is_config_line(char	*line)
 {
-	char	buffer[1024];
-	char	line[MAX_LINE_SIZE];
-	size_t	line_size;
-	ssize_t	bytes_read;
-	ssize_t	i;
+	if (ft_strncmp(line, "NO ", 3)
+		|| ft_strncmp(line, "SO ", 3)
+		|| ft_strncmp(line, "WE ", 3)
+		|| ft_strncmp(line, "AE ", 3)
+		|| ft_strncmp(line, "F ", 2)
+		|| ft_strncmp(line, "C ", 2))
+		return (1);
+	return (0);
+}
 
-	line_size = 0;
-	bytes_read = read(game->map->fd, buffer, sizeof(buffer));
-	while (bytes_read > 0)
+int	find_biggest_row_size(char **grid)
+{
+	int		index;
+	size_t		biggest_size;
+
+	index = 0;
+	biggest_size = 0;
+	while (grid[index])
 	{
-		i = 0;
-		while (i < bytes_read)
+		if (biggest_size < ft_strlen(grid[index]))
+			biggest_size = ft_strlen(grid[index]);
+		index++;
+	}
+	return (biggest_size);
+}
+
+int	normalize_map_size(char **grid)
+{
+	int		index;
+	size_t	biggest_size;
+	char	*filler_string;
+	char	*normalized_string;
+
+	index = 0;
+	biggest_size = find_biggest_row_size(grid);
+	while (index)
+	{
+		if (ft_strlen(grid[index]) < biggest_size)
 		{
-			if (buffer[i] == '\n' || buffer[i] == '\0')
-			{
-				line[line_size] = '\0';
-				printf("%s\n", line);
-				line_size = 0;
-			}
-			else
-				line[line_size++] = buffer[i];
-			i++;
+			filler_string = malloc(biggest_size - ft_strlen(grid[index]));
+			ft_memset(filler_string, ' ', biggest_size - ft_strlen(grid[index]));
+			normalized_string = ft_strjoin(grid[index], filler_string);
+			free(filler_string);
+			free(grid[index]);
+			grid[index] = normalized_string;
 		}
-		if (buffer[bytes_read - 1] == '\n')
-		{
-			printf("end of line\n");
-			break ;
-		}
-		bytes_read = read(game->map->fd, buffer, sizeof(buffer));
+		index++;
 	}
-	if (bytes_read == -1)
+	return (1);
+}
+
+int	parse_map_file(t_game *game, char *line)
+{
+	char	*full_map;
+
+	full_map = "";
+	while (line && line[0] != '\n')
 	{
-		perror("Erro na leitura do arquivo");
-		exit(1);
+		full_map = ft_strjoin(full_map, line);
+		free(line);
+		line = get_next_line(game->map->fd);
 	}
-	if (line_size > 0)
+	game->map->grid = ft_split(full_map, '\n');
+	if (full_map[0] != '\0')
+		free(full_map);
+	return (1);
+}
+
+int	parse_config_file(t_game *game)
+{
+	char	*line;
+
+	line = get_next_line(game->map->fd);
+	while (line)
 	{
-		line[line_size] = '\0';
-		printf("%s\n", line);
+		if (!is_config_line(line) && line[0] != '\n')
+			return (parse_map_file(game, line));
+		free(line);
+		line = get_next_line(game->map->fd);
 	}
+	free(line);
 	return (0);
 }
